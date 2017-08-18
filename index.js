00ba06bbafb6c1a4c5e5c5e7efcdcf9715506ae4
rishipuri <rishipuri@live.in>
2017-08-19 00:05:34 +0100

    Add a simple micro service to generate shield.io badges

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const axios = require('axios');
+
+module.exports = async(req, res) => {
+  const url = `https://api.github.com/repos/${process.env.repo_owner}/${process.env.repo_name}/contents`;
+  const data = await request(url);
+  const urls = dirUrls(data);
+  const count = await dirFileCount(urls);
+  const badgeUrl = `https://img.shields.io/badge/${process.env.badge_subject}-${count}-${process.env.badge_color}.svg`
+
+  res.writeHead(302, {
+    'Location': badgeUrl
+  })
+  res.end();
+}
+
+const request = async(url) => {
+  const { status, data } = await axios({
+    method: 'GET',
+    url: url,
+    responseType: 'json',
+    auth: {
+      username: process.env.gh_username,
+      password: process.env.gh_token
+    }
+  });
+
+  return data;
+}
+
+const dirUrls = (data) => {
+  let urls = data.filter((content) => {
+    if (content.type === 'dir') {
+      return true;
+    }
+
+    return false;
+  }).map((dir) => {
+    return dir.git_url;
+  })
+
+  return urls;
+}
+
+const dirFileCount = async(urls) => {
+  let count = 0;
+
+  for (let url of urls) {
+    const data = await request(url);
+    count += data.tree.length;
+  }
+
+  return count;
+}
