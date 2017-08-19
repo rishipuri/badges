6947aa0039b5a7f537db87baf650a536cdc9797d
rishipuri <rishipuri@live.in>
2017-08-19 01:26:19 +0100

    Return content as svg with proper headers instead of redirecting to shields.io url

--- index.js
+++ index.js
@@ -4,28 +4,36 @@ const axios = require('axios');
 
 module.exports = async(req, res) => {
   const url = `https://api.github.com/repos/rishipuri/til/contents`;
-  const data = await request(url);
+  const data = await request(url, true);
   const urls = dirUrls(data);
   const count = await dirFileCount(urls);
   const badgeUrl = `https://img.shields.io/badge/topics-${count}-green.svg?maxAge=0`;
+  const svg = await request(badgeUrl, false);
 
-  res.writeHead(302, {
-    'Location': badgeUrl,
-    'Cache-Control': 'no-cache'
+  res.writeHead(200, {
+    'Content-Type': 'image/svg+xml',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Expires': 0,
+    'Pragma': 'no-cache'
   });
-  res.end();
+  res.end(svg);
 }
 
-const request = async(url) => {
-  const { status, data } = await axios({
+const request = async(url, gh_auth) => {
+  let req = {
     method: 'GET',
     url: url,
-    responseType: 'json',
-    auth: {
+    responseType: 'json'
+  };
+
+  if (gh_auth) {
+    req['auth'] = {
       username: process.env.gh_username,
       password: process.env.gh_token
-    },
-  });
+    };
+  }
+
+  const { status, data } = await axios(req);
 
   return data;
 }
@@ -48,7 +56,7 @@ const dirFileCount = async(urls) => {
   let count = 0;
 
   for (let url of urls) {
-    const data = await request(url);
+    const data = await request(url, true);
     count += data.tree.length;
   }
 
