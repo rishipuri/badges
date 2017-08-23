9eb6f2e3109433ba288ff50155737e7e34e03e69
rishipuri <rishipuri@live.in>
2017-08-23 20:41:31 +0100

    Add routing

--- index.js
+++ index.js
@@ -1,77 +1,17 @@
+// Config
 require('dotenv').config();
 
-const axios = require('axios');
+// Packages
+const finalhandler = require('finalhandler');
+const Router = require('router');
 
-const {GH_USERNAME, GH_TOKEN} = process.env;
-let auth = false;
+const router = Router();
 
-if (GH_USERNAME && GH_TOKEN) {
-  auth = true;
+// Routes
+const {topics} = require('./routes/til');
+
+router.get('/til/topics', topics);
+
+module.exports = (req, res) => {
+  router(req, res, finalhandler(req, res));
 }
-
-const request = async url => {
-  const req = {
-    method: 'GET',
-    url,
-    responseType: 'json'
-  };
-
-  if (auth) {
-    req.auth = {
-      username: GH_USERNAME,
-      password: GH_TOKEN
-    };
-  }
-
-  const {data} = await axios(req);
-
-  return data;
-};
-
-const dirUrls = data => {
-  const urls = data.filter(content => {
-    if (content.type === 'dir') {
-      return true;
-    }
-
-    return false;
-  }).map(dir => {
-    return dir.git_url;
-  });
-
-  return urls;
-};
-
-const dirFileCount = async urls => {
-  const data = [];
-  let count = 0;
-
-  for (const url of urls) {
-    data.push(request(url));
-  }
-
-  await Promise.all(data).then(values => {
-    values.forEach(dir => {
-      count += dir.tree.length;
-    });
-  });
-
-  return count;
-};
-
-module.exports = async (req, res) => {
-  const url = `https://api.github.com/repos/rishipuri/til/contents`;
-  const data = await request(url);
-  const urls = dirUrls(data);
-  const count = await dirFileCount(urls);
-  const badgeUrl = `https://img.shields.io/badge/topics-${count}-green.svg?maxAge=0`;
-  const svg = await request(badgeUrl, false);
-
-  res.writeHead(200, {
-    'Content-Type': 'image/svg+xml',
-    'Cache-Control': 'no-cache, no-store, must-revalidate',
-    Expires: 0,
-    Pragma: 'no-cache'
-  });
-  res.end(svg);
-};
